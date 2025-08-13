add645d6180fef34aa0963ac24d3ff56c2b9b7ca
marketrackinstitute-code <marketrackinstitute@gmail.com>
2025-08-13 11:37:17 +0530

    Create block-deals.php

---
+++ api/block-deals.php
@@ -0,0 +1,17 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+// Fetch data from NSE India API
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://www.nseindia.com/api/block-deals");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept: application/json"
+]);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
